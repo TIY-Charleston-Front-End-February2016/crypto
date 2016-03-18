d60e15a460ea7b2e4ef88e405ab179bf6e54928d
PiratePowWow <Thi3v3ry>
2016-03-18 16:55:52 -0400

    Added /cryptograms routes

--- src/main/java/com/theironyard/services/CryptogramRepository.java
+++ src/main/java/com/theironyard/services/CryptogramRepository.java
@@ -1,10 +1,15 @@
 package com.theironyard.services;
 
 import com.theironyard.entities.Cryptogram;
+import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by PiratePowWow on 3/17/16.
  */
 public interface CryptogramRepository extends CrudRepository<Cryptogram, Integer> {
+    List<Cryptogram> findBySender(User sender);
+    List<Cryptogram> findByRecipient(User recipient);
 }
