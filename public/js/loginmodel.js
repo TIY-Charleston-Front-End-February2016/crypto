bc32f8ce8c19d28c58acaeac0eb3ad2ec466f98c
Louise Wilson <louisebrookswilson@gmail.com>
2016-03-19 13:22:00 -0400

    working on msg form

---
+++ public/js/loginmodel.js
@@ -0,0 +1,7 @@
+var Backbone = require('backbone');
+module.exports = Backbone.Model.extend({
+  urlRoot: '/login',
+  initialize: function () {
+    // console.log("user model has been created.");
+  }
+});
--- public/js/sendMessageView.js
+++ public/js/sendMessageView.js
@@ -9,21 +9,28 @@ module.exports = Backbone.View.extend({
   template: _.template(tmpl.sendMsgForm),
   initialize: function(){
     console.log('send message view initted');
-  },
-  events: function() {
-    // 'submit .sendMsg': 'sendMsg',
-    // 'click .back': 'goBack'
-  },
-  addOne: function(){
-    var markup = this.template();
-    console.log("TEST", markup);
-    this.$el.html(markup);
-    return this;
+    this.$el.append(this.render());
   },
   render: function(){
     var markup = this.template();
     console.log("TEST", markup);
     this.$el.html(markup);
     return this;
-  }
+  },
+  events: function() {
+    'click .sendMsg': 'createCrypto'
+    // 'click .back': 'goBack'
+  },
+  createCrypto: function(evt){
+  evt.preventDefault();
+  var newCrypto = {
+    recipient: this.$el.find('.recipient').val(),
+    hint: this.$el.find('.hint').val(),
+    message: this.$el.find('.message').val(),
+  };
+  var newMsgModel = new messageModel(newCrypto);
+  this.$el.find('input').val('');
+  newMsgModel.save();
+  this.listenTo(this.collection, 'add', this.addAll);
+},
 });
